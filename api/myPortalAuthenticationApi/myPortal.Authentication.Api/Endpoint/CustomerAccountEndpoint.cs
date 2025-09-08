1a990323f8214984b340292b5206be3b0f47fae9
Jeorge Oliver <jeorgeadonaoliver@gmail.com>
2025-09-08 12:54:39 +0800

    Commit 2

---
+++ api/myPortalAuthenticationApi/myPortal.Authentication.Api/Endpoint/CustomerAccountEndpoint.cs
@@ -0,0 +1,19 @@
+﻿using myPortal.Authentication.Application.Abstraction.Request;
+using myPortal.Authentication.Application.Usecase.Customer.Command.RegisterCustomer;
+
+namespace myPortal.Authentication.Api.Endpoint
+{
+    public static class CustomerAccountEndpoint
+    {
+        public static IEndpointRouteBuilder MapCustomerAccountEndpoint(this IEndpointRouteBuilder app) 
+        {
+            app.MapPost("/auth/register", async (RegisterCustomerCommand command, IRequestDispatcher dispatcher, CancellationToken cancellationToken)=>    
+            { 
+                    var result = await dispatcher.Send(command, cancellationToken);
+                    return Results.Ok(result);
+            });
+
+            return app;
+        }
+    }
+}
