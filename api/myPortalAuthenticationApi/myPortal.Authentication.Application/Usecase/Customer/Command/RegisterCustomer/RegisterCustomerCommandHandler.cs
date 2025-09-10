1ebda9cb22db21941d6fe28a1b2f35758c047dd5
Jeorge Oliver <jeorgeadonaoliver@gmail.com>
2025-09-10 12:52:27 +0800

    Commit 1

--- api/myPortalAuthenticationApi/myPortal.Authentication.Application/Usecase/Customer/Command/RegisterCustomer/RegisterCustomerCommandHandler.cs
+++ api/myPortalAuthenticationApi/myPortal.Authentication.Application/Usecase/Customer/Command/RegisterCustomer/RegisterCustomerCommandHandler.cs
@@ -16,51 +16,60 @@ public class RegisterCustomerCommandHandler : IRequestHandler<RegisterCustomerCo
 
     public async Task<Guid> HandleAsync(RegisterCustomerCommand request, CancellationToken cancellationToken)
     {
+        var validator = new RegisterCustomerCommandValidation(_context.Context);
+        var validationResult = await validator.ValidateAsync(request, cancellationToken);
 
-        var newCustomerId = await _context.ExecuteInTransactionAsync<Guid>(
-            async (db, ct) =>
-            {
-                string uid = string.Empty;
+        if (validationResult.IsValid)
+        {
+           var newCustomerId = await _context.ExecuteInTransactionAsync<Guid>(
+           async (db, ct) =>
+           {
+               string uid = string.Empty;
 
-                try 
-                {
-                    uid = await RegisterCustomerAccountInFirebase(request, ct);
+               try
+               {
+                   uid = await RegisterCustomerAccountInFirebase(request, ct);
 
-                    if (string.IsNullOrEmpty(uid))
-                    {
-                        throw new Exception("Failed to create user in Firebase.");
-                    }
+                   if (string.IsNullOrEmpty(uid))
+                   {
+                       throw new Exception("Failed to create user in Firebase.");
+                   }
 
-                    var customer = new CustomerAccount()
-                    {
+                   var customer = new CustomerAccount()
+                   {
+                       Email = request.Email,
+                       FirstName = request.FirstName,
+                       LastName = request.LastName,
+                       Id = Guid.NewGuid(),
+                       MiddleName = request.MiddletName,
+                       Uid = uid
+                   };
 
-                        Email = request.Email,
-                        FirstName = request.FirstName,
-                        LastName = request.LastName,
-                        Id = Guid.NewGuid(),
-                        MiddleName = request.MiddletName,
-                        Uid = uid
-                    };
+                   db.CustomerAccounts.Add(customer);
 
-                    db.CustomerAccounts.Add(customer);
+                   await db.SaveChangesAsync(ct);
 
-                    await db.SaveChangesAsync(ct);
+                   return customer.Id;
+               }
+               catch (Exception ex)
+               {
+                   //log error
+                   if (!string.IsNullOrEmpty(uid))
+                   {
+                       await FirebaseAuth.DefaultInstance.DeleteUserAsync(uid, ct);
+                   }
+                   throw;
+               }
+           },
+           cancellationToken);
 
-                    return customer.Id;
-                } 
-                catch(Exception ex) 
-                {
-                    //log error
-                    if (!string.IsNullOrEmpty(uid))
-                    {
-                        await FirebaseAuth.DefaultInstance.DeleteUserAsync(uid, ct);
-                    }
-                    throw;
-                }
-            },
-            cancellationToken);
+            return newCustomerId;
+        }
 
-        return newCustomerId;
+        Console.WriteLine(validationResult.ToString());
+
+        return Guid.Empty;
+
     }
 
     private async Task<string> RegisterCustomerAccountInFirebase(RegisterCustomerCommand command, CancellationToken cancellationToken)
@@ -73,15 +82,7 @@ public class RegisterCustomerCommandHandler : IRequestHandler<RegisterCustomerCo
             DisplayName = $"{command.FirstName} {command.LastName}",
         };
 
-        try 
-        {
-            UserRecord userRecord = await auth.CreateUserAsync(userArgs);
-            return userRecord.Uid;
-        }
-        catch (FirebaseAuthException ex)
-        {
-            // Log error
-            return string.Empty;
-        }
+        UserRecord userRecord = await auth.CreateUserAsync(userArgs);
+        return userRecord.Uid;
     }
 }
---
+++ api/myPortalAuthenticationApi/myPortal.Authentication.Application/Usecase/Customer/Command/RegisterCustomer/RegisterCustomerCommandValidation.cs
@@ -0,0 +1,45 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using myPortal.Authentication.Application.Abstraction.Data;
+
+namespace myPortal.Authentication.Application.Usecase.Customer.Command.RegisterCustomer;
+
+public class RegisterCustomerCommandValidation : AbstractValidator<RegisterCustomerCommand>
+{
+    protected readonly IMyPortalDbContext _context;
+
+    public RegisterCustomerCommandValidation(IMyPortalDbContext context)
+    {
+        _context = context;
+
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required.")
+            .EmailAddress().WithMessage("Invalid email format.")
+            .MaximumLength(50).WithMessage("Email must not exceed 50 characters.")
+            .MustAsync(IsEmailExistInFirebase).WithMessage("Email already Exist in Firebase!")
+            .MustAsync(IsEmailExistInDb).WithMessage("Email already Exist in DB!");
+    }
+
+
+    public async Task<bool> IsEmailExistInFirebase(string email, CancellationToken cancellationToken) 
+    {
+        try 
+        {
+            var user = await FirebaseAdmin.Auth.FirebaseAuth.DefaultInstance.GetUserByEmailAsync(email, cancellationToken);
+            if (user is not null)
+                return false;
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            //log error
+            return true;
+        }
+    }
+
+    public async Task<bool> IsEmailExistInDb(string email, CancellationToken cancellationToken)
+    {
+        return  !await _context.CustomerAccounts.AnyAsync(x => x.Email == email, cancellationToken);
+    }
+}
